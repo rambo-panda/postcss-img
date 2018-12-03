4013f793243df5bb9a82c042d821ef0474963acf
rambo <rambojoin@gmail.com>
2018-12-03 15:07:19 +0800

    add switch

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const postcss = require("postcss");
 const urlReg = /url\(['"]?([^'"]+)?['"]?\)/,
     base64Limit = 1024 * 3,
     { existsSync, statSync, readFileSync } = require("fs"),
-    { join, extname } = require("path");
+    { dirname, resolve, extname } = require("path");
 
 const toBase64 = imgSrc =>
     `data:image/${extname(imgSrc).slice(1)};base64,${Buffer.from(
@@ -11,7 +11,7 @@ const toBase64 = imgSrc =>
     ).toString("base64")}`;
 
 module.exports = postcss.plugin(
-    "postcss-webp",
+    "postcss-img",
     (opts = {}) => (root, result) => {
         const newOpts = Object.assign(
             {
@@ -31,33 +31,39 @@ module.exports = postcss.plugin(
                 return;
             }
 
-            const imgSrc = join(__dirname, url);
+            const { base64Limit: $base64Limit, webpClassName } = newOpts;
 
-            if (!existsSync(imgSrc)) {
-                decl.error(`NOT FOUND Image: ${imgSrc}`);
+            if ($base64Limit > 0) {
+                const imgSrc = resolve(dirname(result.opts.from), url);
 
-                return;
+                if (!existsSync(imgSrc)) {
+                    decl.error(`NOT FOUND Image: ${imgSrc}`);
+
+                    return;
+                }
+
+                const { size } = statSync(imgSrc);
+
+                if (size < $base64Limit) {
+                    // eslint-disable-next-line no-param-reassign
+                    decl.value = value.replace(url, toBase64(imgSrc));
+
+                    return;
+                }
             }
 
-            const { size } = statSync(imgSrc);
+            if (webpClassName !== '') {
+                const { parent } = decl;
 
-            if (size < newOpts.base64Limit) {
-                // eslint-disable-next-line no-param-reassign
-                decl.value = value.replace(url, toBase64(imgSrc));
-
-                return;
+                parent.cloneAfter({
+                    selector: `.${newOpts.webpClassName} ${parent.selector}`,
+                    nodes: [
+                        decl.clone({
+                            value: decl.value.replace(url, `${url}.webp`)
+                        })
+                    ]
+                });
             }
-
-            const { parent } = decl;
-
-            parent.cloneAfter({
-                selector: `.${newOpts.webpClassName} ${parent.selector}`,
-                nodes: [
-                    decl.clone({
-                        value: decl.value.replace(url, `${url}.webp`)
-                    })
-                ]
-            });
         });
     }
 );
--- index.test.js
+++ index.test.js
@@ -1,63 +1,81 @@
-var postcss = require('postcss');
+/* eslint-disable max-len */
 
-var plugin = require('./');
+const { resolve } = require('path'),
+    postcss = require('postcss');
+
+const plugin = require('./');
 
 function run(input, output, opts) {
-	return postcss([ plugin(opts) ]).process(input)
-	.then(result => {
-		expect(result.css).toEqual(output);
-		expect(result.warnings().length).toBe(0);
-	});
+    return postcss([ plugin(opts) ]).process(input, {
+        from : resolve('./index.js')
+    })
+        .then(result => {
+            expect(result.css).toEqual(output);
+            expect(result.warnings().length).toBe(0);
+        });
 }
 
+const buildParams = (symbol, type = "-image") => ({
+    test: `a{background${type}:url(${symbol}./test/cc9966.png${symbol})}`,
+    success: `a{background${type}:url(${symbol}data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwAQMAAABtzGvEAAAABGdBTUEAAPQkx/agJQAAAANQTFRFvH5IooWndAAAAA1JREFUGNNjYBgF1AQAAVAAAafi2hYAAAAASUVORK5CYII=${symbol})}`
+});
+
+
 describe('test regexp', () => {
-	it('background-color', () => {
-		const test = 'a{background-color:red }';
-		return run(test, test, { });
-	});
+    it('background-color', () => {
+        const test = 'a{background-color:red }';
+        return run(test, test, { });
+    });
+
+    it('background-image', () => {
+        const { test, success } = buildParams('');
+
+        return run(test, success, {});
+    });
+
+    it('background', () => {
+        const { test, success } = buildParams('', '');
+
+        return run(test, success, {});
+    });
 });
 
 describe('test to base64', () => {
-	const build_param = (symbol) => ({
-		test: `a{background-image:url(${symbol}./test/cc9966.png${symbol})}`,
-		success: `a{background-image:url(${symbol}data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwAQMAAABtzGvEAAAABGdBTUEAAPQkx/agJQAAAANQTFRFvH5IooWndAAAAA1JREFUGNNjYBgF1AQAAVAAAafi2hYAAAAASUVORK5CYII=${symbol})}`,
-	});
+    it('base64', () => {
+        const { test, success } = buildParams('');
 
-	it('base64', () => {
-		const {test, success} = build_param('');
+        return run(test, success, {});
+    });
 
-		return run(test, success, {});
-	});
+    it("base64 '", () => {
+        const { test, success } = buildParams("'");
 
-	it("base64 '", () => {
-		const {test, success} = build_param("'");
+        return run(test, success, {});
+    });
 
-		return run(test, success, {});
-	});
+    it('base64 "', () => {
+        const { test, success } = buildParams('"');
 
-	it('base64 "', () => {
-		const {test, success} = build_param('"');
-
-		return run(test, success, {});
-	});
+        return run(test, success, {});
+    });
 });
 
 describe('test add webp', () => {
-	it('base64', () => {
-		const {test, success} = {
-			test: `a{background-image:url(./test/postcss.jpeg)}`,
-			success: `a{background-image:url(./test/postcss.jpeg)}.webp a{background-image:url(./test/postcss.jpeg.webp)}`,
-		};
+    it('base64', () => {
+        const { test, success } = {
+            test: `a{background-image:url(./test/postcss.jpeg)}`,
+            success: `a{background-image:url(./test/postcss.jpeg)}.webp a{background-image:url(./test/postcss.jpeg.webp)}`
+        };
 
-		return run(test, success, {});
-	});
+        return run(test, success, {});
+    });
 
-	it('base64 multi node', () => {
-		const {test, success} = {
-			test: `a{background-image:url(./test/postcss.jpeg); color:red}`,
-			success: `a{background-image:url(./test/postcss.jpeg); color:red}.webp a{background-image:url(./test/postcss.jpeg.webp)}`,
-		};
+    it('base64 multi node', () => {
+        const { test, success } = {
+            test: `a{background-image:url(./test/postcss.jpeg); color:red}`,
+            success: `a{background-image:url(./test/postcss.jpeg); color:red}.webp a{background-image:url(./test/postcss.jpeg.webp)}`
+        };
 
-		return run(test, success, {});
-	});
+        return run(test, success, {});
+    });
 });
